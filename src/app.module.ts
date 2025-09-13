be4119ff9cdd7351b844b53ddd32983b7132a7e3
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-09-13 11:21:43 +0300

    add(main): ставим mongoose и настраиваем конект для подключения к бд (делаю через докер)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,24 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
